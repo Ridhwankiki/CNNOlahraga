9be968c41392d5ec56c84a5ab974b615b516dd72
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-23 12:53:26 +0700

    Initial Commit

---
+++ CNNOlahraga/CNNOlahragaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNNOlahragaApp.swift
+//  CNNOlahraga
+//
+//  Created by MacBook Pro on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CNNOlahragaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
